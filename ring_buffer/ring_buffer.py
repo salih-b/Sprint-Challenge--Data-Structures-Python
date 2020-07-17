5e09f19ca97c47b00d3cb36f8f073b2a16b03bc1
salih <salihmorta17@gmail.com>
2020-07-17 17:34:26 -0400

    :)

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,41 +1,14 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
-        self.size = 0
+        self.storage = [None]*capacity
+        self.current = 0
 
     def append(self, item):
-        if self.capacity == 0:
-            return
-# when capacity id mmet move oldest(first) item to the back of the list
-        elif self.size < self.capacity:
-            self.storage.append(item)
-            self.size += 1
-# when equal to capacity switch oldest item to the back then remove last item and add new item 
-        # elif self.size == self.capacity:
-        #     self.storage.pop(0)
-        #     self.storage.append(item)
-        #     self.storage.append(self.storage[0])
-        #     self.storage.pop(0)
-        else:
-            self.storage.pop(0)
-            # self.storage.pop()
-            self.storage.append(item)
-            # self.storage.append(self.storage[0])
-            # self.storage.pop(0)
-
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
-        return self.storage
-
-if True == True:
-    test = RingBuffer(4)
-    test.append(2)
-    test.append(45)
-    an1= test.get()
-    test.append(10)
-    an2= test.get()
-    test.append(87)
-    test.append(6)
-    an3= test.get()
-    print(f"TESTING: \n answer one: {an1} \n answer two: {an2} \n removing test returning two: {an3}")
+        return [x for x in self.storage if x is not None]
